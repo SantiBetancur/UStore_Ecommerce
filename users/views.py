a90019e42256e0a6e159a5a1a156981a2ae0de6c
Santiago <rasua2948@gmail.com>
2025-09-08 11:42:32 -0500

    Feat: login and sign up html templates created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View 
 
-# Create your views here.
+class LoginView(View):
+    template_name = 'login.html'
+    def get(self, request):
+        data = {
+            'title': 'Login'
+        }
+        return render(request, self.template_name, data)
+
+class SingInView(View):
+    template_name = 'signin.html'
+    def get(self, request):
+        data = {
+            'title': 'Sing in'
+        }
+        return render(request, self.template_name, data)
