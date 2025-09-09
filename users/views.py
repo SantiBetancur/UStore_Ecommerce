7a34b93c3b924f9d1b8f6e0c8da8fd24a8741c1a
SantiBetancur <santyagobetancurth2004@gmail.com>
2025-09-09 15:49:55 -0500

    feat(templates): add brand icon in the login and signin templates

--- users/views.py
+++ users/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from django.views import View 
 
 class LoginView(View):
-    template_name = 'login.html'
+    template_name = 'pages/login.html'
     def get(self, request):
-        data = {
-            'title': 'Login'
+        context = {
+            'title': 'Ingresar'
         }
-        return render(request, self.template_name, data)
+        return render(request, self.template_name, context)
 
 class SingInView(View):
-    template_name = 'signin.html'
+    template_name = 'pages/signIn.html'
     def get(self, request):
-        data = {
-            'title': 'Sing in'
+        context = {
+            'title': 'Regístrate'
         }
-        return render(request, self.template_name, data)
+        return render(request, self.template_name, context)
