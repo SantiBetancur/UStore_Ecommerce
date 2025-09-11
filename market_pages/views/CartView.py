d29a7bd83fb06db8ae89fb3a2d94172acbe9ef8d
SantiBetancur <santyagobetancurth2004@gmail.com>
2025-09-11 00:47:53 -0500

    feat(cart.html): add redesign and implement template heritage from base.html

--- market_pages/views/CartView.py
+++ market_pages/views/CartView.py
@@ -5,11 +5,23 @@ from market_pages.models.ProductModel import Product
 
 #@login_required
 def view_cart(request):
+    template_name = "pages/cart.html"
     cart, created = Cart.objects.get_or_create(buyer=request.user)
-    return render(request, "cart.html", {"cart": cart})
+    # Bring the username from the session
+    username = request.session.get('username', '') if request.user.is_authenticated else ''
+    context = {
+        'cart': cart,
+        'page_title': 'Mi Carrito - UStore',
+        'username': username,
+        'user': request.user,
+    }
+    return render(request, template_name, context)
 
 #@login_required
 def add_to_cart(request, product_id):
+    template_name = "pages/cart.html"
+    if not request.user.is_authenticated:
+        return redirect("login")
     cart, created = Cart.objects.get_or_create(buyer=request.user)
     product = get_object_or_404(Product, id=product_id)
     item, created = CartItem.objects.get_or_create(cart=cart, product=product)
@@ -20,12 +32,14 @@ def add_to_cart(request, product_id):
 
 #@login_required
 def remove_from_cart(request, item_id):
+    template_name = "pages/cart.html"
     item = get_object_or_404(CartItem, id=item_id, cart__buyer=request.user)
     item.delete()
     return redirect("view_cart")
 
 #@login_required
 def clear_cart(request):
+    template_name = "pages/cart.html"
     cart, created = Cart.objects.get_or_create(buyer=request.user)
     cart.clear()
     return redirect("view_cart")
--- market_pages/views/LandingView.py
+++ market_pages/views/LandingView.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 import logging
 from django.views.generic import TemplateView
-from ..models import Product
+from ..models import Product, Cart
 
 # Create your views here.
 
@@ -13,11 +13,17 @@ logger = logging.getLogger(__name__)
 
 class LandingView(TemplateView):
     template_name = 'pages/landing.html'
+    def dispatch(self, request, *args, **kwargs):
+        # Store the username in the session
+        username = (request.user.name[:20] + '...') if hasattr(request.user, 'name') and len(request.user.name) > 20 else getattr(request.user, 'name', '')
+        request.session['username'] = username
+        return super().dispatch(request, *args, **kwargs)
     def get(self, request):
         # Shorten the username to 20 characters if it's longer
-        username = (request.user.name[:20] + '...') if hasattr(request.user, 'name') and len(request.user.name) > 20 else getattr(request.user, 'name', '')
+        username = request.session.get('username', '') if request.user.is_authenticated else ''
         context = {
             'products': Product.objects.all(),
+            'cart': Cart.objects.get(buyer=request.user) if request.user.is_authenticated else None,
             'default_image': 'static/images/default.png',
             'page_title': 'UStore - Marketplace',
             'username': username,
